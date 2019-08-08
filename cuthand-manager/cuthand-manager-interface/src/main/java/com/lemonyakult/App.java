6edce2da0b093c83ab7591ec9f91005fb3d4e692
LemonGin <legendgin@gmail.com>
2019-08-08 21:28:08 +0800

    initial commit

---
+++ cuthand-manager/cuthand-manager-interface/src/main/java/com/lemonyakult/App.java
@@ -0,0 +1,13 @@
+package com.lemonyakult;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
